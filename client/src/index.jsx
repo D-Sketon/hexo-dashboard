780cfb3deb327ec369efce2e8340e5177f7df755
D-Sketon <2055272094@qq.com>
2023-09-24 15:19:34 +0800

    fix: migrate to hexo

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { HashRouter } from 'react-router-dom'
-// import App from './app';
-// import api from './api';
-// import './style/index.less';
-
-// const url = window.location.href.replace(/^.*\/\/[^\/]+/, '').split('/');
-// const rootPath = url.slice(0, url.indexOf('admin')).join('/');
-// api.init('rest', rootPath + '/admin/api');
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const root = ReactDOM.createRoot(document.getElementById('root'));
-//   root.render(
-//     <React.StrictMode>
-//       <HashRouter>
-//         <App />
-//       </HashRouter>
-//     </React.StrictMode>
-//   );
-// });
-
-import mock from './mock/mock-api';
-import posts from './mock/posts';
-import tagsAndCategories from './mock/tags-and-categories';
-import settings from './mock/settings';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom'
@@ -32,17 +5,44 @@ import App from './app';
 import api from './api';
 import './style/index.less';
 
-api.init(mock, {
-  posts,
-  tagsAndCategories,
-  settings
-})
+const url = window.location.href.replace(/^.*\/\/[^\/]+/, '').split('/');
+const rootPath = url.slice(0, url.indexOf('admin')).join('/');
+api.init('rest', rootPath + '/admin/api');
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
   );
 });
+
+// import mock from './mock/mock-api';
+// import posts from './mock/posts';
+// import tagsAndCategories from './mock/tags-and-categories';
+// import settings from './mock/settings';
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { HashRouter } from 'react-router-dom'
+// import App from './app';
+// import api from './api';
+// import './style/index.less';
+
+// api.init(mock, {
+//   posts,
+//   tagsAndCategories,
+//   settings
+// })
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const root = ReactDOM.createRoot(document.getElementById('root'));
+//   root.render(
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   );
+// });
